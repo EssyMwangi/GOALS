c355bbee66aa4bcb9f0367c3b293c6ead4cc5057
Essy Mwangi <sonnieessy@gmail.com>
2020-03-25 13:37:51 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Goal } from './goal';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
